f1cefd018eaef04230381082210f68c23d4cdc94
chmtrip59 <chmtrip59@gmail.com>
2024-10-15 08:24:16 +0530

    nested class

---
+++ src/in/kgcoding/nested/Car.java
@@ -0,0 +1,13 @@
+package in.kgcoding.nested;
+
+public class Car {// we can make this class which is main class, only as public or default
+    private static class Tyre{//we can make this class public or private or protected or default
+        private double width;
+
+        private double pressur;
+
+        private String material;
+
+
+    }
+}
